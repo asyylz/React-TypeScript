7ec6c6e37e11863cbe6275da3800312a31d79cbc
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-23 12:27:40 +0100

    Added import for useState and NewTodo component, and inserted NewTodo component into App component"

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todos';
 function App() {
   const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
+  const [] =useState()
   return (
     <div>
+      <NewTodo />
       <Todos items={todos} />
     </div>
   );
