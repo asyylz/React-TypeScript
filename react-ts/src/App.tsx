5c7ea877bbd9c90e4d5d1cf1e898e23be258be8b
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-23 11:54:12 +0100

    Updated App.tsx to use Todo model and passed array of Todo objects to Todos component; updated Todos.tsx to expect array of Todo objects instead of strings

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Todos from './components/Todos';
-
+import Todo from './models/todos';
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
   return (
     <div>
-      <Todos items={['Learn React','Learn typescript']} />
+      <Todos items={todos} />
     </div>
   );
 }
